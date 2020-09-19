1ef109d4fcff1f3b879a96847e22438653f5560a
SashaChish <sasha.chihen@gmail.com>
2020-09-19 16:21:03 +0300

    Updates

---
+++ precache-manifest.edcde19b4d14a1aa3a2416ace7ba7fe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a05ded3b94d2d5188bd6f8c13b824c1",
+    "url": "/pictures-of-puzzle/index.html"
+  },
+  {
+    "revision": "8b9539f44faa4b82664f",
+    "url": "/pictures-of-puzzle/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "8c612ba3bf3b09470afc",
+    "url": "/pictures-of-puzzle/static/css/main.b8ba23db.chunk.css"
+  },
+  {
+    "revision": "8b9539f44faa4b82664f",
+    "url": "/pictures-of-puzzle/static/js/2.20193440.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pictures-of-puzzle/static/js/2.20193440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c612ba3bf3b09470afc",
+    "url": "/pictures-of-puzzle/static/js/main.544df801.chunk.js"
+  },
+  {
+    "revision": "dd1757da932301b35d0b",
+    "url": "/pictures-of-puzzle/static/js/runtime-main.d84bf9df.js"
+  }
+]);
